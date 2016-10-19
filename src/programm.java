01df4e00dfa3f9c571644c4dcb9c64347983a8c5
Sseimoja <siimseims@gmail.com>
2016-10-19 15:12:07 +0300

    Initial commit

---
+++ src/programm.java
@@ -0,0 +1,25 @@
+/**
+ * Created by opilane on 19.10.2016.
+ */
+public class programm {public static void main(String[] args) {
+
+    System.out.println("European format: ");
+    String Paev = "Thursday ";
+    System.out.print(Paev);
+    String kuupaev = "19 ";
+    System.out.print(kuupaev);
+    String kuu = "October ";
+    System.out.print(kuu);
+    String aasta = "2016";
+    System.out.println(aasta);
+
+    System.out.println("American format: ");
+    String Pae = "Thursday, ";
+    System.out.print(Pae);
+    String ku = "October ";
+    System.out.print(ku);
+    String kuupae = "19, ";
+    System.out.print(kuupae);
+    String aast = "2016";
+    System.out.print(aast);
+}}
